def6df9f870f8d45d576c07bc07e0176d1108a3b
xiaok <306934150@qq.com>
2019-01-19 15:27:05 +0800

    修改认证中心代码配置

---
+++ common/src/main/java/com/xiaok/common/vo/AuthToken.java
@@ -0,0 +1,51 @@
+﻿package com.xiaok.common.vo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: xiaok
+ * Email: 306934150@qq.com
+ * Date: 2019-01-19
+ * Time: 15:22
+ */
+public class AuthToken {
+
+    /**
+     * 令牌
+     */
+    private String token;
+
+    /**
+     * 有效期
+     */
+    private String expire;
+
+    /**
+     * 刷新token
+     */
+    private String refreshToken;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getExpire() {
+        return expire;
+    }
+
+    public void setExpire(String expire) {
+        this.expire = expire;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
